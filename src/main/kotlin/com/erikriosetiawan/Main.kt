1cce37f5e647587906c811b6b12b63358859d0f1
erikrios <erikriosetiawan49@gmail.com>
2019-08-24 17:22:47 +0700

    Unit return type declaration

---
+++ src/main/kotlin/com/erikriosetiawan/Main.kt
@@ -0,0 +1,15 @@
+package com.erikriosetiawan
+
+/**
+ * This function below are equivalent with this code
+ * fun printHello(name: String?) {
+        if (name != null) {
+            println("Hello $name")
+        }
+    }
+ */
+fun printHello(name: String?) {
+    if (name != null) {
+        println("Hello $name")
+    }
+}
