a703687a4dbf6aa39c566d141d6775186da909d3
erikrios <erikriosetiawan49@gmail.com>
2019-08-24 17:38:05 +0700

    Equlity of values

--- src/main/kotlin/com/erikriosetiawan/Main.kt
+++ src/main/kotlin/com/erikriosetiawan/Main.kt
@@ -18,3 +18,34 @@ fun printHello(name: String?) {
  * Single expression functions
  */
 fun double(x: Int): Int = x * 2
+
+/**
+ * String interpolation
+ * In java:
+ * int num = 10
+ * String s = "i = " + num;
+ */
+val num = 10
+val s = "i = $num"
+
+/**
+ * Nullable references
+ */
+fun main() {
+    /**
+     *  var a: String = "abc"
+        a = null
+     */
+   var a: String? = "abc"
+    a = null
+
+    /**
+     * Equlity of values
+     */
+    val b = 5
+    val c = 10
+
+    if (b == c) {
+        println("$b equals with $c")
+    }
+}
